3647db4360f011e3783e4a6ca1e7d54b63cd473f
Prymak-Marharyta <prymak.marharyta@student.sumdu.edu.ua>
2025-03-30 14:27:07 +0300

    lab3

---
+++ завд3_лаб3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  int numofseconds;
+  int hours, minutes, seconds;
+
+  printf("Enter the number of seconds: ");
+  scanf("%d", &numofseconds);
+
+  if (numofseconds < 0) {
+    printf("The number of seconds cannot be negative");
+  } else {
+    hours = numofseconds / 3600;
+
+    int remainingseconds = numofseconds % 3600;
+
+    minutes = remainingseconds / 60;
+
+    seconds = remainingseconds % 60;
+
+    printf("%d seconds = %d hours, %d minutes, %d seconds\n",
+        numofseconds, hours, minutes, seconds);
+  }
+
+  return 0;
+}
